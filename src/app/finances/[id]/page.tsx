5af7881dfb190abc3dfac315ade4acc41b065175
SirCQQ <gatucristian@gmail.com>
2025-01-30 09:27:13 +0200

    feat: fix small placeholder for finance page

--- src/app/finances/[id]/page.tsx
+++ src/app/finances/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { PageView } from "@/components/ui/page-view";
 
-export default function FianceById({ params }) {
+export default function FianceById({ params }: { params: { id: string } }) {
   return (
     <PageView className="flex items-center justify-center">
       Finance by id page {JSON.stringify(params, null, 2)}
--- src/app/finances/page.tsx
+++ src/app/finances/page.tsx
@@ -3,7 +3,7 @@ import { FianncesList } from "./(components)/finances-list";
 export default async function Page() {
   return (
     <main>
-      <div className="h-20 w-full p-2 text-center text-lg leading-[80px]">
+      <div className="h-10 w-full p-2 text-center text-lg leading-[20px] sm:h-20 sm:leading-[80px]">
         Place holder for small utilities like create new, filter by last change,
         owned or invited
       </div>
